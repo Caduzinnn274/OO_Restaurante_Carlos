729426eef94ee484afc3895a40a28abd905274a7
Carlos Bressan <105678823+Caduzinnn274@users.noreply.github.com>
2024-06-10 10:22:22 -0300

    Update atividade4.py-

--- modelos/atividade4.py
+++ modelos/atividade4.py
@@ -1,42 +1,51 @@
-#Atividade 1
-class Pessoa:
-    pessoas = []
+]# #Atividade 1# #Atividade 1
+# class Pessoa:
+#     pessoas = []
 
-    def __init__(self, nome, idade, profissao):
-        self.nome = nome.title()
-        self.idade = idade.upper()
-        self.profissao = profissao.upper()
+#     def __init__(self, nome, idade, profissao):
+#         self.nome = nome.title()
+#         self.idade = idade.upper()
+#         self.profissao = profissao.upper()
 
-        Pessoa.pessoas.append(self)
+#         Pessoa.pessoas.append(self)
 
-    def __str__(self):
-        return f"{self.nome} | {self.idade} | {self.profissao}"
+#     def __str__(self):
+#         return f"{self.nome} | {self.idade} | {self.profissao}"
 
-    @classmethod
-    def listar_pessoas(cls):
-        print(f"{'Nome da Pessoa'.ljust(20)} | {'Idade'.ljust(20)} | {'Profissão'.ljust(20)}") 
-        for pessoa in cls.pessoas:
-            nome_justificado = pessoa.nome.ljust(20)
-            idade_justificada = pessoa.idade.ljust(20)
-            profissao_justificado = pessoa.profissao.ljust(20)
-            print(f"{nome_justificado} | {idade_justificada} | {profissao_justificado}")
+#     @classmethod
+#     def listar_pessoas(cls):
+#         print(f"{'Nome da Pessoa'.ljust(20)} | {'Idade'.ljust(20)} | {'Profissão'.ljust(20)}") 
+#         for pessoa in cls.pessoas:
+#             nome_justificado = pessoa.nome.ljust(20)
+#             idade_justificada = pessoa.idade.ljust(20)
+#             profissao_justificado = pessoa.profissao.ljust(20)
+#             print(f"{nome_justificado} | {idade_justificada} | {profissao_justificado}")
 
-    def Aniversario(self):
-        self.idade += 1
-    @property
-    def Saudacao(self):
-        return(f"Saudações {self.profissao} {self.nome}")
+#     def Aniversario(self):
+#         self.idade += 1
+#     @property
+#     def Saudacao(self):
+#         return(f"Saudações {self.profissao} {self.nome}")
     
         
 
 
 
 
-pessoa_1 = Pessoa('Carlos','17', 'Mecânico')
-pessoa_2 = Pessoa('Cristiano','16', 'Engenheiro')
+# pessoa_1 = Pessoa('Carlos','17', 'Mecânico')
+# pessoa_2 = Pessoa('Cristiano','16', 'Engenheiro')
+# print(pessoa_1.Saudacao)
+# print(pessoa_2.Saudacao)
+
+
+# Pessoa.listar_pessoas()
 
 
 
-Pessoa.listar_pessoas()
+
+#Atividade 2
+
+
+   
 
 
